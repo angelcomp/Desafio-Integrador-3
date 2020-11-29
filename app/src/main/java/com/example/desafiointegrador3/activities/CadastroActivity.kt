f79175d2de981a40abf498480fa9e863a0ff0de5
Angelica <santosangelicassp@gmail.com>
2020-11-29 14:13:24 -0300

    Criando layout e funcionalidade na tela de cadastro

--- app/src/main/java/com/example/desafiointegrador3/activities/CadastroActivity.kt
+++ app/src/main/java/com/example/desafiointegrador3/activities/CadastroActivity.kt
@@ -1,12 +1,40 @@
 package com.example.desafiointegrador3.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desafiointegrador3.R
+import com.example.desafiointegrador3.utils.Usuario
+import kotlinx.android.synthetic.main.activity_cadastro.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        iv_btnCadastro.setOnClickListener {
+            callMain(getInformationUser())
+        }
+    }
+
+    //Pega as informações do usuario
+    fun getInformationUser(): Usuario {
+        val nome = et_nameCadastro.text.toString()
+        val email = et_emailCadastro.text.toString()
+        val senha = et_senhaCadastro.text.toString()
+
+        return Usuario(nome, email, senha)
+    }
+
+    //Chamando a MainActivity
+    fun callMain(usuario: Usuario) {
+        var intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("user", usuario)
+        startActivity(intent)
     }
 }
